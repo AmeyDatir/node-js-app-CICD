575271c0bbc05e94f87be1768e49c9b5ebb6e21e
AmeyDatir <ameydatir21@gmail.com>
2026-03-09 13:59:11 +0530

    change in app.js file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from jenkins, pm2 deployed Node.js app through jenkins cicd');
+  res.send('Hello from jenkins, pm2 deployed Node.js app through jenkins cicd pipeline and this is myy second change');
 });
 
 app.listen(port, () => {
